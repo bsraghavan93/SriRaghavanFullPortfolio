943fd814e52529e78cd9ca8ff3103fccbbe69412
bsraghavan93 <bsraghavan93@gmail.com>
2026-04-02 16:11:43 -0400

    Add Rubik font to layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
-import { Geist, Geist_Mono } from "next/font/google"
+import { Geist, Geist_Mono, Rubik } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils";
 
 const geist = Geist({subsets:['latin'],variable:'--font-sans'})
+
+const rubik = Rubik({ subsets: ['latin'], variable: '--font-rubik' })
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
@@ -20,7 +22,7 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", geist.variable)}
+      className={cn("antialiased", fontMono.variable, "font-sans", geist.variable, rubik.variable)}
     >
       <body>
         <ThemeProvider>{children}</ThemeProvider>
